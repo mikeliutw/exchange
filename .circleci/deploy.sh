db3070b33a796c68b42eb9464344c4c75e570511
Mike <taiwanliu@gmail.com>
2022-01-27 10:30:42 +0800

    new config

---
+++ .circleci/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# container name
+container=exchange
+docker_command="docker exec -i $container"
+
+# shutdown the laravel app
+$docker_command php artisan down
+
+# update PHP dependencies
+$docker_command composer install  --no-interaction --no-dev --prefer-dist
+# --no-interaction Do not ask any interactive question
+# --no-dev  Disables installation of require-dev packages.
+# --prefer-dist  Forces installation from package dist even for dev versions.
+
+# update database
+$docker_command php artisan migrate --force
+# --force  Required to run when in production.
+
+# cache boost configuration and routes
+$docker_command php artisan cache:clear
+$docker_command php artisan config:cache
+$docker_command php artisan route:cache
+
+# horizon
+$docker_command php artisan horizon:purge
+$docker_command php artisan horizon:terminate
+$docker_command php artisan queue:restart
+
+# rise from the ashes
+$docker_command php artisan up
+
+echo 'Deploy finished.'
